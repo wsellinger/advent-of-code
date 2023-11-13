e3a31a994adc4c812d2c8208178ed0ce35531e2f
William Sellinger <wsellinger@gmail.com>
2023-11-13 13:56:50 -0500

    Day 01 B Completed

--- aoc-2015-tests/Year_2015_Tests/Day_01_Tests.cs
+++ aoc-2015-tests/Year_2015_Tests/Day_01_Tests.cs
@@ -14,10 +14,19 @@ namespace UnitTests.Year_2015_Tests
         [InlineData("))(", -1)]
         [InlineData(")))", -3)]
         [InlineData(")())())", -3)]
-        public void Given_ValidInput_Returns_ExpectedResult(string input, long expected)
+        public void A_Given_ValidInput_Returns_ExpectedResult(string input, long expected)
         {
-            var actual = Year_2015.Day_01(input);
+            var actual = Year_2015.Day_01_A(input);
             Assert.Equal(expected, actual);
-        }
-    }
+		}
+
+		[Theory]
+		[InlineData(")", 1)]
+		[InlineData("()())", 5)]
+		public void B_Given_ValidInput_Returns_ExpectedResult(string input, long expected)
+		{
+			var actual = Year_2015.Day_01_B(input);
+			Assert.Equal(expected, actual);
+		}
+	}
 }
